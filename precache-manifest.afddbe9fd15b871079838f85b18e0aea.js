3a95f845066fa2cd13592c8cad64404a44e64dc8
Mari Mattos <marianasmmattos@gmail.com>
2020-07-04 18:50:00 -0300

    Updates

---
+++ precache-manifest.afddbe9fd15b871079838f85b18e0aea.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4757122da190faa8dbe72709ff4dd60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ae1df356d92ee2d6109",
+    "url": "/static/js/2.de2fbbfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de2fbbfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f14325eb12ed6ae5d2e",
+    "url": "/static/js/main.f17060ea.chunk.js"
+  },
+  {
+    "revision": "8ddb24a34f8d04c08687",
+    "url": "/static/js/runtime-main.a41ee944.js"
+  },
+  {
+    "revision": "3fbf5a84d4f02dda562cd0a2775e6727",
+    "url": "/static/media/Logo.3fbf5a84.svg"
+  },
+  {
+    "revision": "0a3498ce4aef3c3fab8240cc0c3d05e3",
+    "url": "/static/media/becca.0a3498ce.jpg"
+  },
+  {
+    "revision": "55a1f6f9dc10272c6024f30e18c8181c",
+    "url": "/static/media/daniel.55a1f6f9.jpg"
+  },
+  {
+    "revision": "ae7020fb57ac4d400a0993c907174642",
+    "url": "/static/media/filipez.ae7020fb.jpg"
+  },
+  {
+    "revision": "8fe26068663ade315b807d4188b887be",
+    "url": "/static/media/jonathas.8fe26068.jpg"
+  },
+  {
+    "revision": "0e3591f28f6dedda8fb858a3dc95a531",
+    "url": "/static/media/me.0e3591f2.jpg"
+  },
+  {
+    "revision": "e79735f7b46ec91748312ad61c50a52f",
+    "url": "/static/media/pp.e79735f7.jpg"
+  },
+  {
+    "revision": "cba66d3b57acc6257fde58a27cc88308",
+    "url": "/static/media/sasa.cba66d3b.jpg"
+  },
+  {
+    "revision": "7cc4a22904037b240932687125e01a97",
+    "url": "/static/media/techdojo.7cc4a229.svg"
+  }
+]);
