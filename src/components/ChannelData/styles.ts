c6e7ea84fe0e774ebc4a8c3a05c3a205b4e98ecc
Mari Mattos <marianasmmattos@gmail.com>
2020-07-05 12:10:37 -0300

    commit

--- src/components/ChannelData/styles.ts
+++ src/components/ChannelData/styles.ts
@@ -34,7 +34,7 @@ export const Messages = styled.div`
 
 export const InputWrapper = styled.div`
     width: 100%;
-    height: 60px;
+    height: 46px;
     background:  var(--secondary);
     display: flex;
     align-items: center;   
